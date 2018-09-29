f4eb695f6a3670a101fa0e67e2c75868e278df3a
Trey Pernell <trey@piazza.com>
2018-09-28 19:35:25 -0700

    Gracefully end connection once seeded

--- seed.js
+++ seed.js
@@ -2,15 +2,11 @@ const cassandra = require('cassandra-driver');
 const async = require('async');
 const fs = require('fs');
 const client = new cassandra.Client({contactPoints: ['127.0.0.1:9042']});
-const _ = require('underscore');
 const Promise = require('bluebird');
-// const eachAsync = Promise.promisify(_.each);
-const forEachAsync = Promise.promisify(async.forEachOf);
-console.log(forEachAsync);
 
 const readFileAsync = Promise.promisify(fs.readFile);
-//TODO after interview - research and test a way of inserting multiple columns where necessary.
 
+console.time('timer');
 client.execute("DROP TABLE IF EXISTS demo.homes")
   .then((result) => {
     return client.execute("DROP KEYSPACE IF EXISTS demo");
@@ -22,33 +18,27 @@ client.execute("DROP TABLE IF EXISTS demo.homes")
     return client.execute("CREATE TABLE demo.homes (home_id int PRIMARY KEY, image map<text, text>)");
   })
   .then((result) => {
-    seedDB();
+    return seedDB();
   })
   .then((result) => {
-    // client.shutdown();
-    // console.log('success');
-    // console.log(result);
+    client.shutdown();
+    console.timeEnd('timer')
+    console.log('success');
+    console.log(result);
   })
   .catch((err) => {
-    // client.shutdown();
+    client.shutdown();
     console.log('failure');
     console.log(err);
   })
 
 async function seedDB (factor /* TODO - Refactor to take factor as ENV variable */) {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 2; i++) {
     let chunk = [];
     for (let j = 0; j < 50; j++) {
-      chunk.push(j + (i * 50)); //REMEMBER TO REFACTOR THE MATHEMATICALS HERE
+      chunk.push(j + (i * 50));
     }
-    console.log('yoooo');
     await Promise.all(chunk.map(fileNum => seedFile(fileNum)))
-    // .then((response) => {
-    //   console.log('promise completed at: ', i)
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // })
     console.log(`chunked from: ${i * 50} to ${49 + (i * 50)}`);
   }
 }
@@ -60,11 +50,10 @@ function seedFile (fileNum) {
     for (let home_id in data) {
       var homeQuery = `INSERT INTO demo.homes (home_id, image) values (?, ?)`;
       var id = parseInt(home_id) + (fileNum * 2000);
-      // console.log(id, home_id, fileNum);
       var homeValues = [id, data[home_id]];
+      
       await client.execute(homeQuery, homeValues, {prepare: true})
         .then((response) => {
-          // console.log('successfully seeded at: ', id, home_id, fileNum);
         })
         .catch((err) => {
           console.log(err);
